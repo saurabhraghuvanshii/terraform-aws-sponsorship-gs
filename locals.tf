1953f28d7f7038333c4c13ec4bf489ba7848be5d
jayfranco999 <jaideepfrancis999@gmail.com>
2024-11-13 14:25:33 +0530

    chore(updatecli):Track and update azure outbound ips
    Signed-off-by: jayfranco999 <jaideepfrancis999@gmail.com>

--- locals.tf
+++ locals.tf
@@ -3,12 +3,24 @@ locals {
   aws_account_id = "326712726440"
   region         = "us-east-2"
   our_az         = format("${local.region}%s", "b")
-  outbound_ips = { # source : https://reports.jenkins.io/jenkins-infra-data-reports/azure-net.json
-    "trusted.ci.jenkins.io"              = ["104.209.128.236", "172.177.128.34", "172.210.175.108", "172.210.170.228"] # permanent agent of update_center2
-    "privatek8s.jenkins.io"              = ["20.65.63.127"]                                                            # VPN VM
-    "infracijenkinsioagents1.jenkins.io" = ["20.122.14.108", "20.186.70.154"]                                          # Terraform management and Docker-packaging build
-    "private.vpn.jenkins.io"             = ["172.176.126.194"]                                                         # connections routed through the VPN
+  ## Tracked bu updatecli from the following source: https://reports.jenkins.io/jenkins-infra-data-reports/azure-net.json
+  ## Note: we use strings with space separator to manage type changes in updatecli's HCL parser
+  # permanent agent of update_center2
+  outbound_ips_trusted_ci_jenkins_io = "104.209.128.236 172.177.128.34 172.210.175.108 172.210.170.228"
+  # Infra.ci / Release CI controllers
+  outbound_ips_privatek8s_jenkins_io = "20.65.63.127"
+  # Terraform management and Docker-packaging build
+  outbound_ips_infracijenkinsioagents1_jenkins_io = "20.122.14.108 20.186.70.154"
+  # Connections routed through the VPN
+  outbound_ips_private_vpn_jenkins_io = "172.176.126.194"
+
+  outbound_ips = {
+    "trusted.ci.jenkins.io"              = split(" ", local.outbound_ips_trusted_ci_jenkins_io)
+    "privatek8s.jenkins.io"              = split(" ", local.outbound_ips_privatek8s_jenkins_io)
+    "infracijenkinsioagents1.jenkins.io" = split(" ", local.outbound_ips_infracijenkinsioagents1_jenkins_io)
+    "private.vpn.jenkins.io"             = split(" ", local.outbound_ips_private_vpn_jenkins_io)
   }
+
   common_tags = {
     "scope"      = "terraform-managed"
     "repository" = "jenkins-infra/terraform-aws-sponsorship"
